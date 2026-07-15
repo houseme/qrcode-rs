14cfe071c3ce8dd22350f7e85cc24f12dc0ad1b7
houseme <housemecn@gmail.com>
2026-07-15 11:26:10 +0800

    docs: add const version example

--- examples/const_version.rs
+++ examples/const_version.rs
@@ -1,11 +1,20 @@
-use qrcode_rs::{ConstVersion, EcLevel, QrCode, Version};
+use qrcode_rs::{ConstVersion, ConstVersionEncoder, EcLevel, Encoder, QrCode, Version};
 
 fn main() {
-    const VERSION: Version = ConstVersion::<5>::VALUE;
+    const VERSION_5: Version = ConstVersion::<5>::VALUE;
 
-    let code = QrCode::with_const_version::<5, _>(b"https://example.com/fixed-version", EcLevel::M).unwrap();
+    let direct =
+        QrCode::with_const_version::<5, _>(b"https://example.com/const-version", EcLevel::M).unwrap_or_else(|err| {
+            eprintln!("{err}");
+            std::process::exit(1);
+        });
+    let encoded =
+        ConstVersionEncoder::<5>::new(EcLevel::M).encode(b"https://example.com/const-version").unwrap_or_else(|err| {
+            eprintln!("{err}");
+            std::process::exit(1);
+        });
 
-    println!("version: {:?}", VERSION);
-    println!("width: {}", code.width());
-    println!("dark modules: {}", code.dark_modules().count());
+    assert_eq!(direct.version(), VERSION_5);
+    assert_eq!(encoded.version(), VERSION_5);
+    println!("fixed version: {:?}, width: {}", VERSION_5, encoded.width());
 }
